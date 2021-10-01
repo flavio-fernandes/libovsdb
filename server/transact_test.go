6034de8990a6fe2cec3849e0fbe7d485a1313cb3
Flavio Fernandes <flaviof@redhat.com>
2021-10-01 03:10:19 +0000

    testing multiple ops in a single transaction

--- server/transact_test.go
+++ server/transact_test.go
@@ -125,3 +125,126 @@ func TestMutateOp(t *testing.T) {
 		},
 	}, gotUpdate)
 }
+
+func TestMultipleOps(t *testing.T) {
+	defDB, err := model.NewDBModel("Open_vSwitch", map[string]model.Model{
+		"Bridge": &bridgeType{}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	schema, err := getSchema()
+	if err != nil {
+		t.Fatal(err)
+	}
+	ovsDB := NewInMemoryDatabase(map[string]*model.DBModel{"Open_vSwitch": defDB})
+	o, err := NewOvsdbServer(ovsDB, DatabaseModel{
+		Model: defDB, Schema: schema})
+	require.Nil(t, err)
+
+	bridgeUUID := uuid.NewString()
+	m := mapper.NewMapper(schema)
+
+	bridge := bridgeType{
+		Name: "a_bridge_to_nowhere",
+		Ports: []string{
+			"port1",
+			"port10",
+		},
+	}
+	bridgeRow, err := m.NewRow("Bridge", &bridge)
+	require.Nil(t, err)
+
+	res, updates := o.Insert("Open_vSwitch", "Bridge", bridgeUUID, bridgeRow)
+	_, err = ovsdb.CheckOperationResults([]ovsdb.OperationResult{res}, []ovsdb.Operation{{Op: "insert"}})
+	require.Nil(t, err)
+
+	err = o.db.Commit("Open_vSwitch", updates)
+	require.NoError(t, err)
+
+	var ops []ovsdb.Operation
+	var op ovsdb.Operation
+
+	op = ovsdb.Operation{
+		Table: "Bridge",
+		Where: []ovsdb.Condition{
+			ovsdb.NewCondition("_uuid", ovsdb.ConditionEqual, ovsdb.UUID{GoUUID: bridgeUUID}),
+		},
+		Op: ovsdb.OperationMutate,
+		Mutations: []ovsdb.Mutation{
+			// *ovsdb.NewMutation("ports", ovsdb.MutateOperationInsert, ovsdb.UUID{GoUUID: "portA"}),
+			// *ovsdb.NewMutation("ports", ovsdb.MutateOperationDelete, []ovsdb.UUID{{GoUUID: "port10"}}),
+			*ovsdb.NewMutation("ports", ovsdb.MutateOperationDelete, ovsdb.UUID{GoUUID: "port10"}),
+		},
+	}
+	ops = append(ops, op)
+
+	op = ovsdb.Operation{
+		Table: "Bridge",
+		Where: []ovsdb.Condition{
+			ovsdb.NewCondition("_uuid", ovsdb.ConditionEqual, ovsdb.UUID{GoUUID: bridgeUUID}),
+		},
+		Op: ovsdb.OperationMutate,
+		Mutations: []ovsdb.Mutation{
+			*ovsdb.NewMutation("ports", ovsdb.MutateOperationInsert, ovsdb.UUID{GoUUID: "portA"}),
+			// *ovsdb.NewMutation("ports", ovsdb.MutateOperationDelete, ovsdb.UUID{GoUUID: "port10"}),
+		},
+	}
+	ops = append(ops, op)
+
+	op = ovsdb.Operation{
+		Table: "Bridge",
+		Where: []ovsdb.Condition{
+			ovsdb.NewCondition("_uuid", ovsdb.ConditionEqual, ovsdb.UUID{GoUUID: bridgeUUID}),
+		},
+		Op: ovsdb.OperationMutate,
+		Mutations: []ovsdb.Mutation{
+			*ovsdb.NewMutation("ports", ovsdb.MutateOperationInsert, ovsdb.UUID{GoUUID: "portB"}),
+		},
+	}
+	ops = append(ops, op)
+
+	op = ovsdb.Operation{
+		Table: "Bridge",
+		Where: []ovsdb.Condition{
+			ovsdb.NewCondition("_uuid", ovsdb.ConditionEqual, ovsdb.UUID{GoUUID: bridgeUUID}),
+		},
+		Op: ovsdb.OperationMutate,
+		Mutations: []ovsdb.Mutation{
+			*ovsdb.NewMutation("ports", ovsdb.MutateOperationInsert, ovsdb.UUID{GoUUID: "portB"}),
+			*ovsdb.NewMutation("ports", ovsdb.MutateOperationInsert, ovsdb.UUID{GoUUID: "portC"}),
+		},
+	}
+	ops = append(ops, op)
+
+	results, updates := o.transact("Open_vSwitch", ops)
+	require.Len(t, results, len(ops))
+	for _, result := range results {
+		assert.Equal(t, "", result.Error)
+	}
+
+	err = o.db.Commit("Open_vSwitch", updates)
+	require.NoError(t, err)
+
+	oldPorts, err := ovsdb.NewOvsSet([]ovsdb.UUID{{GoUUID: "port1"}, {GoUUID: "port10"}})
+	assert.Nil(t, err)
+	newPorts, err := ovsdb.NewOvsSet([]ovsdb.UUID{{GoUUID: "port1"}, {GoUUID: "portA"}, {GoUUID: "portB"}, {GoUUID: "portC"}})
+	assert.Nil(t, err)
+
+	assert.Equal(t, ovsdb.TableUpdates{
+		"Bridge": ovsdb.TableUpdate{
+			bridgeUUID: &ovsdb.RowUpdate{
+				Old: &ovsdb.Row{
+					"_uuid": ovsdb.UUID{GoUUID: bridgeUUID},
+					"name":  "a_bridge_to_nowhere",
+					"ports": oldPorts,
+				},
+				New: &ovsdb.Row{
+					"_uuid": ovsdb.UUID{GoUUID: bridgeUUID},
+					"name":  "a_bridge_to_nowhere",
+					"ports": newPorts,
+				},
+			},
+		},
+	}, updates)
+
+}
